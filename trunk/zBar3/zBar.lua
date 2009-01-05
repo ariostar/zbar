f9c81863e5f488cc903e9df4ea31b0e72833a7a2
zero.flamy <zero.flamy@4c917ca8-d543-0410-91fa-a1bb168b883a>
2009-01-05 09:55:42 +0000

    zBar3: - Initial

---
+++ trunk/zBar3/zBar.lua
@@ -0,0 +1,119 @@
+--[[ Create ]]
+zBar3 = CreateFrame("Frame")
+zBar3:RegisterEvent("PLAYER_LOGIN")
+
+--[[ Tables ]]
+zBar3.plugins = {}
+zBar3.bars    = {}
+zBar3.buttons = {}
+
+--[[ Common functions ]]
+function zBar3:print(msg, r, g, b)
+	DEFAULT_CHAT_FRAME:AddMessage(msg, r, g, b)
+end
+
+--[[ Event ]]
+function zBar3:OnEvent()
+	if event == "PLAYER_LOGIN" then
+		-- self init
+		zBar3:Init()
+		-- plugins init
+		for k,v in ipairs(zBar3.plugins) do
+			if v.Load then v:Load() end
+		end
+		-- bars init
+		for name, bar in pairs(zBar3.bars) do
+			bar:Reset()
+		end
+		-- hooks
+		zBar3:Hook()
+		-- welcome message
+		zBar3:print("zBar3 v"..zBar3.version.." Loaded :: Author - "..zBar3.author.. " :: type /zbar",0.0,1.0,0.0)
+	end
+end
+zBar3:SetScript("OnEvent", zBar3.OnEvent)
+
+--[[ Register Sub Addon ]]
+-- the order of registeration will also effect when Initialize !
+function zBar3:AddPlugin(obj, afterWho)
+	-- insert behine the afterWho
+	if afterWho then
+		for k,v in ipairs(self.plugins) do
+			if v == afterWho then
+				table.insert(self.plugins, k, obj)
+				return
+			end
+		end
+	end
+	-- otherwise just append it
+	table.insert(self.plugins, obj)
+end
+
+--[[ Register a Bar ]]
+function zBar3:AddBar(bar)
+	self.bars[bar:GetName()] = bar
+
+	-- inherit functions
+	setmetatable(bar, {__index = zBarT})
+
+end
+
+--[[ Addon Init ]]
+function zBar3:Init()
+  -- version
+	self.version = GetAddOnMetadata("zBar3", "Version")
+	self.author  = GetAddOnMetadata("zBar3", "Author")
+
+  -- data
+	zBar3Data = zBar3Data or { version = zBar3.version,}
+
+  -- Lite mode state
+  self.lite = select(4, GetAddOnInfo("zBar3Lite"))
+
+  -- function that does nothing
+  self.NOOP = function() end
+
+  -- hidden frame
+  self.hiddenFrame = CreateFrame("Frame")
+  self.hiddenFrame:Hide()
+
+  -- class
+	self.class = select(2, UnitClass("player"))
+end
+
+function zBar3:Hook()
+	-- remove that '?' thing
+	for id, name in pairs(self.buttons) do
+		local hotkey = _G[name.."HotKey"]
+		if hotkey and hotkey:GetText() == RANGE_INDICATOR then
+			hotkey:SetText("  ")
+		end
+	end
+	RANGE_INDICATOR = "  "
+
+	-- hook scripts for all action buttons
+	local name, bar, button
+	for name, bar in pairs(self.bars) do
+		if bar:GetID() <= 10 then
+			for id = 1, NUM_ACTIONBAR_BUTTONS do
+				button = _G[self.buttons[bar:GetName()..id]]
+				if button then
+					button:SetMovable(true)
+					-- set button scripts
+					button:SetScript("OnEnter",function(self)
+						if zTab:FreeOnEnter(self) then return end
+						self:GetParent():SetAlpha(1)
+						if zBar3Saves.hideTip then return end
+						ActionButton_SetTooltip(self)
+					end)
+					button:SetScript("OnLeave",function(self)
+						local bar = self:GetParent()
+						bar:SetAlpha(zBar3Saves[bar:GetName()].alpha)
+						if zBar3Saves.hideTip then return end
+						GameTooltip:Hide()
+					end)
+				end
+			end
+		end
+	end
+end
---
+++ trunk/zBar3/zBarT.lua
@@ -0,0 +1,224 @@
+local _G = getfenv(0)
+--[[
+	Functions for Derivates
+--]]
+
+-- template for common functions of bars
+zBarT = CreateFrame("Frame",nil,UIParent,"SecureHandlerStateTemplate")
+
+--[[ reset profile, scale, position to DEFAULT for any bar ]]
+function zBarT:Reset(resetsaves)
+	local db = zBar3Data
+	local name = self:GetName()
+	
+	-- reset profile
+	if resetsaves or not db[name] then
+		db[name] = zBar3:GetDefault(self,"saves")
+		db[name].pos = zBar3:GetDefault(self,"pos")
+	end
+	-- reset scale
+	self:SetScale(db[name]["scale"] or 1)
+	self:GetTab():SetScale(self:GetScale())
+	-- reset alpha
+	self:SetAlpha(db[name].alpha or 1)
+	-- name plate
+	if db[name].label then self:GetLabel():Show() end
+
+	-- remove tab from master tab's cortege list
+	if self:GetTab().master then
+		tDeleteItem(self:GetTab().master.cortege, self:GetTab():GetName())
+		self:GetTab().master = nil
+	end
+	-- remove tab's corteges
+	if self:GetTab().cortege then
+		for n, name in ipairs(self:GetTab().cortege) do
+			_G[name]:ClearAllPoints()
+			table.remove(self:GetTab().cortege, n)
+			_G[name].master = nil
+		end
+	end
+
+	-- reset position
+	local pos = db[name].pos or zBar3:GetDefault(self, "pos")
+	self:GetTab():ClearAllPoints()
+	if type(pos[2]) == "string" then
+		self:GetTab():SetPoint(pos[1],UIParent,pos[2],pos[3],pos[4])
+	else
+		self:GetTab():SetPoint(pos[1],UIParent,pos[1],pos[2],pos[3])
+	end
+	self:ClearAllPoints()
+	self:SetPoint("TOP",self:GetTab(),"BOTTOM",0,0)
+
+	-- update all
+	self:UpdateVisibility()
+	self:UpdateButtons()
+	self:UpdateLayouts()
+	self:UpdateHotkeys()
+	self:UpdateAutoPop()
+end
+
+function zBarT:UpdateVisibility()
+	if zBar3Data[self:GetName()].hide then
+    self:Hide()
+  else
+    self:Show()
+	end
+	if zBar3Data[self:GetName()].hideTab then
+    self:GetTab():Hide()
+  else
+    self:GetTab():Show()
+	end
+end
+
+function zBarT:UpdateAutoPop()
+	local db = zBar3Data[self:GetName()]
+	local header = self:GetHeader()
+	UnregisterStateDriver(header, "visibility")
+	if not db.inCombat then
+		header:Show()
+	else
+		if "autoPop" == db.inCombat then
+			RegisterStateDriver(header, "visibility", "[combat][harm,nodead]show;hide")
+		elseif "autoHide" == db.inCombat then
+			RegisterStateDriver(header, "visibility", "[combat][harm,nodead]hide;show")
+		end
+	end
+end
+
+--[[ update buttons, hide unwanted buttons ]]
+function zBarT:UpdateButtons()
+	local button
+	local db = zBar3Data[self:GetName()]
+
+	if db.max == 0 then return end
+
+	for i =  1, db.max or NUM_ACTIONBAR_BUTTONS do
+		button = self:GetButton(i)
+		assert(button)
+		if i <= (db.num or 1) then
+			if _G[button:GetName().."AutoCast"] then
+				if PetActionBarFrame.showgrid > 0 or GetPetActionInfo(i) then
+					button:Show()
+				end
+			elseif not button.action 
+			or (button:GetAttribute("showgrid") > 0 or HasAction(button.action)) then
+				button:Show()
+			end
+			--button:SetAttribute("showstates", nil)
+			button:SetAttribute("statehidden", nil)
+		else
+			button:Hide()
+			--button:SetAttribute("showstates", "!*")
+			button:SetAttribute("statehidden", true)
+		end
+	end
+end
+
+--[[ enable / disable hotkey text shown for bar ]]
+function zBarT:UpdateHotkeys()
+	local hotkey
+
+	for i = 1 , zBar3Data[self:GetName()].max or NUM_ACTIONBAR_BUTTONS do
+		hotkey = _G[ (zBar3.buttons[self:GetName()..i] or "?? ").."HotKey"]
+		if hotkey then
+			if zBar3Data[self:GetName()].hideHotkey then
+				hotkey:Hide()
+				hotkey.zShow = hotkey.Show
+				hotkey.Show = zBar3.NOOP
+			elseif hotkey.zShow then
+				hotkey.Show = hotkey.zShow
+				if hotkey:GetText() ~= RANGE_INDICATOR then
+					hotkey:Show()
+				end
+			end
+		end
+	end
+end
+
+function zBarT:GetHeader()
+	local id = self:GetID()
+	local header = _G["zBar3Header"..id]
+	if header then return header end
+
+	header = CreateFrame("Frame", "zBar3Header"..id, UIParent,"SecureHandlerStateTemplate")
+	header:SetID(id)
+
+  self:SetParent(header)
+  self:GetTab():SetParent(header)
+
+	return header
+end
+
+--[[ get localized bar name as label ]]
+function zBarT:GetLabel()
+	local label = _G[self:GetName().."Label"]
+
+	if not label then
+		label = self:GetHeader():CreateFontString(self:GetName().."Label", "ARTWORK", "GameFontGreen")
+		label:SetPoint("BOTTOM", self:GetTab(), "TOP", 0, 0)
+		label:SetText( zBar3.loc.Labels[self:GetName()] or self:GetName() )
+	end
+
+	return label
+end
+
+function zBarT:GetButton(id)
+	return _G[zBar3.buttons[self:GetName()..id]]
+end
+
+-- overwrite this if needed
+function zBarT:GetChildSizeAdjust(attachPoint)
+	return 0, 0
+end
+
+-- get tab of bar, create if not exist
+function zBarT:GetTab()
+	local id = self:GetID()
+	local tab = _G["zTab"..id]
+	if tab then return tab end
+
+	tab = CreateFrame("Button", "zTab"..id, UIParent, "zBarTabTemplate")
+	tab:SetID(id)
+	tab:RegisterForDrag("LeftButton")
+	tab:RegisterForClicks("LeftButtonUp", "RightButtonUp")
+	tab:SetScript("OnDragStart", function(self) zTab:OnDragStart(self) end)
+	tab:SetScript("OnDragStop", function(self) zTab:OnDragStop(self) end)
+	
+	tab:SetFrameRef('bar', self)
+	tab:Execute([[bar = self:GetFrameRef('bar')]])
+	
+	tab:SetAttribute("_onclick", [[
+		if button == 'RightButton' then
+			CallMethod('OnMenu')
+		elseif button == 'LeftButton' then
+			if bar:IsShown() then
+				bar:Hide()
+				bar:SetAttribute('statehidden', true)
+			else
+				bar:Show()
+				bar:SetAttribute('statehidden', nil)
+			end
+		end
+	]])
+	
+	tab.OnMenu = function(self, unit, button)
+		if not zBarOption then zBar3:print("Option not been loaded") return end
+		zBarOption:Openfor(this.bar)
+	end
+
+	tab:SetAttribute("_onenter", [[
+		if bar:GetAttribute('statehidden') then bar:Show() end
+	]])
+	tab:SetAttribute("_onleave", [[
+		if bar:GetAttribute('statehidden') then bar:Hide() end
+	]])
+
+	tab:SetScale(self:GetScale())
+	tab:SetFrameLevel(self:GetFrameLevel() + 5)
+
+	tab:SetWidth(self:GetWidth())
+
+	tab.bar = self
+
+	return tab
+end
