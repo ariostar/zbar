43d10dfdaf296c9c69ad4a8cd9adab61de22a0bc
zero.flamy <zero.flamy@4c917ca8-d543-0410-91fa-a1bb168b883a>
2009-01-14 19:02:31 +0000

    zBar3 - fix: bugs of vehicle - fix: bugs of cat stealth - fix: bugs of grid update - fix: bugs of button facade support - fix: many other bugs

---
+++ tags/zBar3.0.7/zBar3/defaults.lua
@@ -0,0 +1,80 @@
+local _G = getfenv(0)
+--[[
+	Usage: zBar3:GetDefault("zMainBar")
+	or : zBar3:GetDefault("zMainBar", "pos")
+--]]
+function zBar3:GetDefault(name, key, subkey)
+	local k,v,m,n
+
+	if type(name) == "table" then
+		name = name:GetName()
+	end
+
+	-- copy
+	local set = {}
+	for k,v in pairs(zBar3.defaults[name]) do
+		if type(v) == "table" then
+			set[k] = {}
+			for m,n in pairs(v) do
+				set[k][m] = n
+			end
+		else
+			set[k] = v
+		end
+	end
+
+	-- common values
+	for k,v in pairs(zBar3.defaults["*"]) do
+		if set[k] then
+			for m,n in pairs(v) do
+				if set[k][m] == nil then set[k][m] = n end
+			end
+		end
+	end
+
+	if key then
+		set = set[key]
+		if subkey then set = set[subkey] end
+	end
+
+	return set
+end
+
+-- zBar3 defaults
+zBar3.defaults = {
+	["*"] = {saves = {num = 12, inset = 0, layout = "line", linenum = 2, alpha = 1,},},
+	["zExBar1"] = { saves = { num=6,},
+		pos = {"CENTER",0,0},
+	},
+	["zShadow1"] = { saves = {num=6, max=6,},
+		pos = {"CENTER",72,0},
+	},
+	["zExBar2"] = { saves = { num=6,},
+		pos = {"CENTER",-72,0},
+	},
+	["zShadow2"] = { saves = {num=6, max=6,},
+		pos = {"CENTER",-144,0},
+	},
+
+	["zMultiR2"] = {
+		saves = {linenum = 1,},
+		pos ={"BOTTOMRIGHT",-47,500},
+	},
+	["zMultiR1"] = {
+		saves = {linenum = 1,},
+		pos ={"BOTTOMRIGHT",-7,500},
+	},
+	["zMultiBR"] = {
+		saves = {linenum = 12, hideTab=true,},
+		pos ={"BOTTOM",-100,138},
+	},
+	["zMultiBL"] = {
+		saves = {linenum = 12, hideTab=true,},
+		pos ={"BOTTOM",-100,102},
+	},
+
+	["zCastBar"] = {
+		saves = {num = 2, max = 2, hideTab=true,},
+		pos={"BOTTOM",0,205-24},
+	},
+}
---
+++ tags/zBar3.0.7/zBar3/locale/en.lua
@@ -0,0 +1,80 @@
+if not zBar3.loc then
+	zBar3.loc = {}
+	local loc = zBar3.loc
+
+	loc.Labels = {
+		zMultiR1 = "Right", zMultiR2 = "Left", zMultiBR = "BRight", zMultiBL = "BLeft",
+		zMainBar = "Main", zPetBar = "Pet", zStanceBar = "Stance", zBagBar = "Bag",
+		zMicroBar = "Micro", zXPBar = "XP", zCastBar = "Cast", zPossessBar = "Possess",
+		zExBar1 = "Extra1", zShadow1 = "Shadow1", zExBar2 = "Extra2", zShadow2 = "Shadow2",
+	}
+
+	loc.Option = {
+		Title = "zBar Option",
+		SelectBar	= "Select Bar",
+		Attribute	= "Attribute",
+		Layout		= "Layout",
+		InCombat = "In Combat",
+		Commons = "Commons",
+		Reset    = "Reset Bar",
+
+		Hide = "Hide",
+		Label = "Label",
+		Lock = "Lock",
+		HotKey = "noHotkey",
+
+		AutoPop = "AutoPop",
+		AutoHide = "AutoHide",
+
+		Suite1 = "Suite1",
+		Suite2 = "Suite2",
+		Circle = "Circle",
+		Free = "Free",
+		Invert = "Invert",
+
+		HideTip = "Hide Tooltip",
+		LockButtons = "Lock All Buttons",
+		HideGrid = "Hide Empty Buttons",
+		PageTrigger = "Auto Page",
+		CatStealth = "Swich page when stealth for Druid cat form",
+
+		Scale = "Scale: ",
+		Inset = "Button Spacing",
+		Num = "Num of Buttons",
+		NumPerLine = "Num per Line",
+		Alpha = "Alpha",
+	}
+
+	loc.Tips = {
+		Hide		= "Show / Hide this bar",
+		Label	= "Show / Hide name of this bar",
+		Lock		= "Lock bar and hide tab",
+		HotKey			= "Show / Hide Hotkeys for buttons",
+
+		AutoPop	= "Pop up when Targeting Enemy or In Combat, Hide otherwise",
+		AutoHide	= "Auto Hide in combat",
+
+		Suite1		= "Classic Shapes\nTry addjust Button Spacing when num is 12",
+		Suite2		= "Fancy Shapes\nTry addjust Button Spacing when num is 12",
+		Circle		= "Ringed around. \nSet the Button Spacing to change radius",
+		Free			= "Moves when dragging and hold Ctrl+Alt+Shift\nScale on mouse wheel",
+		Invert		= "Mirror it horizontally, left to right, right to left",
+
+		HideTip		= "Show / Hide Tooltips",
+		LockButtons	= "Lock All Buttons",
+		HideGrid		= "Hide Grid of Empty Buttons",
+		PageTrigger = "Auto switch page of MainBar While Alt key down or has Assistable target",
+
+		Scale			= "Drag slider, Change Scale to appropriate size",
+		Inset			= "Adjust button spacing between each other",
+		Num				= "Number of Buttons to show in bar\nIf this is Extra Bar, then the Shadow bar of this will Change it's Max Num",
+		NumPerLine	= "Set Number of Buttons per Line\nSort Buttons Vertical / Horizontal / Multi Line",
+		Alpha			= "Transparent when mouse leave\nNormal when enter",
+	}
+
+	--[[ Bindings ]]--
+	BINDING_HEADER_ZEXBUTTON = "zBar2 - Extra Buttons"
+	for i = 1, 24 do
+		setglobal("BINDING_NAME_CLICK zExButton"..i..":LeftButton", "zExButton"..i)
+	end
+end
