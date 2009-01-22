7f9e3d61235a12a513d6a66756a511e4256773b0
zero.flamy <zero.flamy@4c917ca8-d543-0410-91fa-a1bb168b883a>
2009-01-22 02:06:07 +0000

    zBar3 - When in vehicle mode, all but extra bars will be hidden. And the Blizzard's VehicleMenuBar will show up.

--- trunk/v3/zBar3FullMode/zVehicleBar.lua
+++ trunk/v3/zBar3FullMode/zVehicleBar.lua
@@ -1,11 +1,10 @@
 local _G = getfenv(0)
 
-CreateFrame("Frame","zVehicleBar",UIParent,"SecureHandlerBaseTemplate")
+CreateFrame("Frame","zVehicleBar",UIParent,"SecureFrameTemplate")
 zBar3:AddPlugin(zVehicleBar, zMainBar)
 
 local OnMainMenuBarHide = function(self)
-	if self.state == "vehicle" then
-		zVehicleBar.state = self.state
+	if MainMenuBar.state == "vehicle" then
 		zMicroBar:UnHook()
 	end
 end
@@ -13,41 +12,33 @@ end
 local OnVehicleMenuBarHide = function(self)
 	if zVehicleBar.state == "vehicle" then
 		zVehicleBar.state = "player"
-		zMicroBar:UnHook()
-		for i=1,zMicroBar:GetNumButtons() do
-			zMicroBar:GetButton(i):SetParent(zMicroBar)
-			zMicroBar:GetButton(i):Show()
-		end
+		zMicroBar:UpdateButtons()
 		zMicroBar:UpdateLayouts()
-		zMicroBar:GetButton(1):zClearAllPoints()
-		zMicroBar:GetButton(1):zSetPoint("BOTTOM")
 	end
 end
-local AfterUpdateTalentButton = function()
+
+local postVehicleArt = function(self)
+	zVehicleBar.state = "vehicle"
 	zMicroBar:Hook()
 end
 
+local postPlayerArt = function(self)
+	--zMicroBar:Hook()
+end
+
 function zVehicleBar:Load()
-	--CharacterMicroButton.SetParent = zMicroButtonSetParent
-	hooksecurefunc(MainMenuBar, "Hide", OnMainMenuBarHide)
-	hooksecurefunc(VehicleMenuBar, "Hide", OnVehicleMenuBarHide)
-	hooksecurefunc("MainMenuBar_ToVehicleArt",AfterUpdateTalentButton)
+	if zMicroBar then
+		hooksecurefunc(MainMenuBar, "Hide", OnMainMenuBarHide)
+		hooksecurefunc(VehicleMenuBar, "Hide", OnVehicleMenuBarHide)
+		hooksecurefunc("MainMenuBar_ToVehicleArt",postVehicleArt)
+		hooksecurefunc("MainMenuBar_ToPlayerArt",postPlayerArt)
+	end
 	for name,bar in pairs(zBar3.bars) do
-		bar:SetParent(zBar3)
+		if (bar:GetID() > 4) then -- not extra bars
+			bar:SetParent(zBar3)
+		end
 	end
 	RegisterStateDriver(zBar3, "visibility", "[target=vehicle,exists]hide;show")
-end
-
-function zVehicleBar:ToVehicleArt()
-	if zMicroBar then
-		zMicroBar:UnHook()
-	end
-end
-
-function zVehicleBar:ToPlayerArt()
-	if zMicroBar then
-		zMicroBar:Hook()
-	end
 end
 
 function zVehicleBar:Test()
